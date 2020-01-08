1228338e2128c324cf57d33523a50005b28d42d2
MDolega20 <mateusz@dolega.me>
2020-01-08 11:25:16 +0100

    .

--- days_to_xlsx.py
+++ days_to_xlsx.py
@@ -3,14 +3,24 @@ import json
 import time
 from datetime import datetime
 
-summary = xlsxwriter.Workbook('output.xlsx')
-worksheet = summary.add_worksheet()
+path_output = './resources/output.xlsx'
+workbook = xlsxwriter.Workbook(path_output)
+worksheet = workbook.add_worksheet()
 
 working_days = []
 
-print("Reading jsons")
-with open('./resources/input_xlsx.json') as json_file:
-    working_days = json.load(json_file)
+print("Czytanie jsons")
+path_input = './resources/input_xlsx.json'
+with open(path_input) as json_file:
+    working_days = json.load(json_file)["days"]
 print("Wczytano " + str(len(working_days)) + " dni pracujących")
 
+print("Tworzenie struktury arkusza")
 
+worksheet.merge_range('A1:D1', 'Merged Range', merge_format)
+
+worksheet.write('A1', 'Hello world')
+
+
+print("Zamykanie arkusza")
+workbook.close()
